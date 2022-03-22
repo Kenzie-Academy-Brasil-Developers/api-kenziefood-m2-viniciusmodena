d5049c561960e6b2a305467338eaad4221afae93
Osmar Faria <osm.faria@gmail.com>
2022-03-22 10:51:49 -0700

    fix: user img right header

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { Api } from "./controller/api.js"
 
 // Usuario já foi registrado, este são os dados:
 
-// const user = {
-//     name: 'Modena',
-//     email: 'modena@gmail.com',
-//     password: 'equipe1'
-// }
+const user = {
+    name: 'Modena',
+    email: 'modena@gmail.com',
+    password: 'equipe1'
+}
+
+const token = await Api.userLogin(user)
 
